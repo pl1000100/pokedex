acd5abbd1b9f745e7c3cbd63796a145e436a9cb5
pl1000100 <olszynski97@gmail.com>
2025-02-08 19:57:26 +0100

    Add exit and help

---
+++ command_help.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	for _, c := range getCommands() {
+		fmt.Printf("%s: %s\n", c.name, c.description)
+	}
+	return nil
+}
--- repl.go
+++ repl.go
@@ -1,11 +1,57 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		clean_text := cleanInput(scanner.Text())
+		if len(clean_text) == 0 {
+			continue
+		}
+		command, exists := getCommands()[clean_text[0]]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	lower := strings.ToLower(text)
 	words := strings.Fields(lower)
 	return words
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+
 }
